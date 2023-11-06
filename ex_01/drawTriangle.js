f93468a743b38b159dc563a980900fcac6fe591f
Razmo <you@example.com>
2023-11-06 13:58:52 +0100

    EX 01 WITHOUT ERROR HANDLING

---
+++ ex_01/drawTriangle.js
@@ -0,0 +1,15 @@
+
+function drawTriangle (triangle){
+    let triangl="";
+    for (let i=1; i<=triangle; i++) {
+        
+        for (let j=1; j<=i; j++); {
+       triangl += "$";
+        }
+    console.log(triangl);
+    }   
+ 
+}
+drawTriangle(6);
+
+
